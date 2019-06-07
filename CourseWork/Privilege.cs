455ad2f848d1ed22f5bd0ee014feb516f8639b3a
Leonov Artem <leonovru@yandex.ru>
2019-06-08 00:03:02 +0300

    Add method 'Command' in 'Privilege'

--- CourseWork/Privilege.cs
+++ CourseWork/Privilege.cs
@@ -8,6 +8,8 @@ namespace CourseWork
 
         public Privilege(MySqlConnection connection) => this.connection = connection;
 
+        private MySqlCommand Command(string sql) => new MySqlCommand(sql, connection);
+
         public bool AddUser(string login, string password, Position position)
         {
             string sql = $"SELECT add_new_user('{login}', '{password}', ";
@@ -25,26 +27,22 @@ namespace CourseWork
                     break;
             }
 
-            MySqlCommand command = new MySqlCommand(sql, connection);     // объект для выполнения SQL-запроса
-            return (bool)command.ExecuteScalar();
+            return (bool)Command(sql).ExecuteScalar();
         }
         public bool DeleteUser(string login, string password)
         {
             string sql = $"SELECT delete_user('{login}', '{password}');";
-            MySqlCommand command = new MySqlCommand(sql, connection);
-            return (bool)command.ExecuteScalar();
+            return (bool)Command(sql).ExecuteScalar();
         }
         public bool UserExist(string login, string password)
         {
             string sql = $"SELECT check_user_existence('{login}', '{password}');";
-            MySqlCommand command = new MySqlCommand(sql, connection); 
-            return (bool)command.ExecuteScalar();
+            return (bool)Command(sql).ExecuteScalar();
         }
         public Position GetUserPosition(string login, string password)
         {
             string sql = $"SELECT get_user_position('{login}', '{password}');";
-            MySqlCommand command = new MySqlCommand(sql, connection);
-            string pos = command.ExecuteScalar().ToString();
+            string pos = Command(sql).ExecuteScalar().ToString();
 
             Position position = Position.NotSelected;
             switch(pos)
